1a9295e89b45ed7a7d6ef2a03b8f3986ef3e6e9f
steve <smarden1@gmail.com>
2013-08-14 20:47:50 -0400

    better git ignore

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@
 # create symbolic links for all dot files in this directory
 # if file already exists, make a backup
 
-files=`find . -name ".*" -maxdepth 1 | grep -v "\\.$" | grep -v "git"`
+files=`find . -name ".*" -maxdepth 1 | grep -v "\\.$" | grep -v "git$"`
 backup_directory=backup_`date "+%Y_%m_%d%n"`
-current_directory=`pwd`
 
 mkdir -p $backup_directory
 
@@ -12,5 +11,7 @@ for file in $files; do
 	[ -f ~/$file ] &&
 		echo "$file already exists. moving to backup directory" &&
 		mv ~/$file $backup_directory/$file
-	ln -s $current_directory/$file  ~/$file
+	ln -s `pwd`/$file  ~/$file
 done
+
+git config --global core.excludesfile `pwd`/.gitignore
